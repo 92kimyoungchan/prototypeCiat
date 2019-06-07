ca72145f009188a39775cc2b53b2adeadeba5d82
ppiyo5 <ppiyo5@gmail.com>
2019-06-07 12:30:59 +0900

    Fix (index) index html fix

---
+++ js/owl2.js
@@ -0,0 +1,43 @@
+	$(document).ready(function() {
+
+
+		$("#owlc2").owlCarousel({
+
+			navigation : true, // Show next and prev buttons
+			slideSpeed : 300,
+			paginationSpeed : 400,
+			margin:20,
+			loop:true,
+			dots:true,
+			dotsEach:true,
+			responsiveClass:true,
+			responsive: {
+				  0:{
+			            items:1,
+			            nav:true
+			        },
+			        481:{
+			            items:2,
+			            nav:true
+			        },
+			        768:{
+			            items:4,
+			            nav:true
+			        },
+			        992:{
+			            items:5,
+			            nav:true,
+			            loop:true
+			        }
+			},
+			 singleItem : true,
+			 items : 5,
+       nav : true, // Show next and prev buttons
+       navText: [
+				   "<img src='images/main/left-arrow.png'>",
+				   "<img src='images/main/right-arrow.png'>"
+				   ]
+
+		});
+
+		});
