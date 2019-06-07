11385c24b331e95b1c72a02456635b50a70918fa
ppiyo5 <ppiyo5@gmail.com>
2019-06-07 20:30:54 +0900

    fix index html + css 변경, 글꼴추가

---
+++ js/owl.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+
+  $(".owlc2").owlCarousel({
+
+    navigation : true, // Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    margin:20,
+    loop:true,
+    dots:false,
+    dotsEach:true,
+    responsiveClass:true,
+    responsive: {
+        0:{
+                items:1,
+                nav:true
+            },
+            481:{
+                items:2,
+                nav:false
+            },
+            768:{
+                items:3,
+                nav:false
+            },
+            992:{
+                items:5,
+                nav:true,
+                loop:true
+            }
+    },
+    singleItem : true,
+     items : 5,
+        nav : true, // Show next and prev buttons
+        navText: [
+         "<img src='images/main/left-arrow.png'/>",
+         "<img src='images/main/right-arrow.png'/>"
+          ]
+
+
+  });
+
+
+  });
